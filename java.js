0889f3a620858ba464f347d2a7a756a0ff49a737
SoulSho <rafaelwangsabesari@gmail.com>
2024-07-26 00:17:31 -0300

    Add files via upload

---
+++ java.js
@@ -0,0 +1,7 @@
+// Navigation Toggle for Mobile
+const navToggle = document.querySelector('.nav-toggle');
+const navMenu = document.querySelector('.nav-menu');
+
+navToggle.addEventListener('click', () => {
+    navMenu.classList.toggle('nav-menu-visible');
+});
